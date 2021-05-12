821f4c16c56b81cddd0f91066df19e35d41941ac
vinayak <admin@itvoyager.com>
2021-05-12 18:01:06 +0900

    Project's Initial Commit

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+/*Kartero App Configuration*/
+
+var krms_driver_config ={	
+	'ApiUrl':"https://fleet.whereisly.com/whereisly/api",						
+	'DialogDefaultTitle':"Whereisly",    
+	'APIHasKey':"",
+	'debug': false
+};
